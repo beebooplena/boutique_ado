b98617c7fadd8575357890c3c8e0feaa8cc7eb65
beebooplena <lena_kath@hotmail.com>
2022-07-31 17:54:57 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,6 +9,7 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
@@ -38,6 +39,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
+    
     
     grand_total = delivery + total
 
